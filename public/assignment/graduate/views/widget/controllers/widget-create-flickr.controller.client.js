5e6ce13c3aaf1691b012f8779ac27eeb7df971d7
gautam0209 <gautam0209@ccs.neu.edu>
2017-06-18 22:42:45 -0400

    removingLogs

--- public/assignment/graduate/views/widget/controllers/widget-create-flickr.controller.client.js
+++ public/assignment/graduate/views/widget/controllers/widget-create-flickr.controller.client.js
@@ -23,7 +23,6 @@
             flickrService
                 .searchPhotos(searchTerm)
                 .then(function(response) {
-                    console.log(response.data);
                     data = response.data.replace("jsonFlickrApi(","");
                     data = data.substring(0,data.length - 1);
                     data = JSON.parse(data);
